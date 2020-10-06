c74bb88d8f58b8d33673a60cf46db8a7eacad276
Evan Sheehan <evan.s@hey.com>
2020-10-06 11:28:46 -0600

    Set up Nunjucks
    Configure some basic Nunjucks templates for the sketchbook.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
+const path = require("path");
+
+const nunjucks = require("nunjucks");
 const polka = require("polka");
 
+nunjucks.configure(path.join("src", "templates"));
+
 polka().get("/", (req, res) => {
-  res.end("<h1>Hello, world</h1>");
+  res.end(nunjucks.render("index.njk"));
 }).listen(8080, err => {
   if (err) throw err;
   console.log("> Running on http://localhost:8080/");
