06b78d1b4bec1b49bc52590adab18d84888fb50a
Evan Sheehan <evan.s@hey.com>
2020-10-07 13:47:41 -0600

    Rendering something approximating Tarbell's sand.stroke
    I've got an implementation that is beginning to look a bit like Jared Tarbell's original
    sand.stroke. One thing I'm not currently doing is switching colors the way he did. It
    would probably be wise not to draw everything all at once, either.

--- sketches/sand_stroke.js
+++ sketches/sand_stroke.js
@@ -9,17 +9,24 @@ a {
   color: white;
 }`;
 
-var size = 600;
+var size = 600,
+    numLines = 12,
+    iterations = 7,
+    iter = 0;
 
 var colors = [];
-
-var numLines = 12;
 
 var lines = [];
 
 function init() {
   for (let i = 0; i < numLines; i++) {
-    lines[i] = [random(size), random(colors)];
+    const y = random(size);
+
+    lines[i] = sandstroke(
+        createVector(0, y),
+        createVector(size, y)
+      )
+      .color(random(colors));
   }
 }
 
@@ -35,14 +42,92 @@ function setup() {
     color("#FFD400")
   ];
 
+  colors.forEach(c => c.setAlpha(16));
+
   init();
-  noLoop();
 };
 
 function draw() {
   for (let i = 0; i < numLines; i++) {
-    const [y, c] = lines[i];
-    stroke(red(c), green(c), blue(c), 255);
-    line(0, y, size, y);
+    lines[i]();
   }
+
+  iter++;
+  if (iter > iterations) noLoop();
 };
+
+function mouseClicked() {
+  init();
+  clear();
+  loop();
+}
+
+function sandstroke(start, end) {
+  var amplitude = 400,
+      myColor = color(0, 0, 0, 16),
+      grains = 200;
+
+  function draw() {
+    const vec = p5.Vector.sub(end, start),
+          l = vec.mag();
+
+    const originColor = color(myColor.toString("rgba"));
+    originColor.setAlpha(alpha(myColor) * 3);
+
+    // "sg" comes from Jared Tarbell's original Processing sketch. I'm not sure
+    // what it means, but it is used in the sine function that spreads out the
+    // "sand grains".
+    let sg = random(0.01, 0.1);
+
+    translate(start);
+    rotate(vec.heading());
+
+    for (let i = 0; i < l; i++) {
+      const t = i / l,
+            w = sg / grains,
+            x = lerp(0, l, t),
+            c = color(myColor.toString("rgba"));
+
+      stroke(originColor);
+      point(x, 0);
+
+      for (let j = 0; j < grains; j++) {
+        const y = amplitude * sin(j * w);
+
+        c.setAlpha(255 * (0.1 - j / (grains * 10 + 10)));
+        stroke(c);
+
+        point(x, y);
+        point(x, -y);
+      }
+
+      // Perturb sg so we get a different value out of our call to sin(). These
+      // magic numbers come from Jared Tarbell's implementation.
+      sg += random(-0.042, 0.042);
+
+      // Clamp the value between [-0.3, 0.3]. Again the magic numbers are from
+      // Tarbell.
+      sg = min(max(-0.3, sg), 0.3);
+    }
+  }
+
+  draw.amplitude = function(v) {
+    if (arguments.length < 1) return amplitude;
+    amplitude = v;
+    return draw;
+  };
+
+  draw.color = function(v) {
+    if (arguments.length < 1) return myColor;
+    myColor = v;
+    return draw;
+  };
+
+  draw.grains = function(v) {
+    if (arguments.length < 1) return grains;
+    grains = v;
+    return draw;
+  };
+
+  return draw;
+}
